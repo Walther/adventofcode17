c9261da94f479ac861d5f8429b14033af779b4ca
Veeti Haapsamo <veeti.haapsamo@gmail.com>
2017-12-15 08:05:04 +0200

    Solve day 15 part 2

--- 15.js
+++ 15.js
@@ -30,6 +30,24 @@ const part1 = input => {
   return judge;
 };
 
+const part2 = input => {
+  let valueA = input[0];
+  let valueB = input[1];
+  let judge = 0;
+  for (let i = 0; i < 5e6; i++) {
+    do {
+      valueA = genA(valueA);
+    } while (valueA % 4 !== 0);
+    do {
+      valueB = genB(valueB);
+    } while (valueB % 8 !== 0);
+    if (byteify(valueA) === byteify(valueB)) {
+      judge++;
+    }
+  }
+  return judge;
+};
+
 assert.equal(genA(65), 1092455);
 assert.equal(genA(1092455), 1181022009);
 assert.equal(genB(8921), 430625591);
@@ -37,4 +55,4 @@ assert.equal(genB(430625591), 1233683848);
 
 const input = [679, 771];
 const test = [65, 8921];
-part1(input);
+console.log([part1(input), part2(input)]);
