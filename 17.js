c815aa801ca61653a1b12196a90425050cb1b035
Veeti Haapsamo <veeti.haapsamo@gmail.com>
2017-12-17 12:44:51 +0200

    Optimize day 17 part 1

--- 17.js
+++ 17.js
@@ -1,21 +1,17 @@
 const assert = require("assert");
 
-const rotate = (array, n) => {
-  n = n % array.length;
-  return array.slice(n, array.length).concat(array.slice(0, n));
-};
-
-const spinlock = input => {
-  let array = [0];
-  for (let i = 1; i <= 2017; i++) {
-    array = rotate(array, input);
-    array.push(i);
-  }
-  return array[0];
-};
-
 test = 3;
 input = 363;
+
+const spinlock = step => {
+  let array = [0];
+  let currentPos = 0;
+  for (let i = 1; i <= 2017; i++) {
+    currentPos = (step + currentPos) % array.length + 1;
+    array.splice(currentPos, 0, i);
+  }
+  return array[array.indexOf(2017) + 1 % array.length];
+};
 
 assert.equal(spinlock(test), 638);
 console.log(spinlock(input));
